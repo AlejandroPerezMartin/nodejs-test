d579e850ba22128ae0ec81dd7ae5aaf3e5427f7d
Alejandro <aleperez92@gmail.com>
2015-05-24 16:12:47 +0200

    Added form simple form validation

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 /**
- * NodeJS app for testing
- * @author Alejandro Perez
+ * NodeJS app for testing using Express and Jade templating engine
+ *
+ * @author Alejandro Perez Martin
+ * @autor_url http://linkedin.com/in/aleperez92
+ * @repository https://github.com/AlejandroPerezMartin/nodejs-test.git
  */
 
 /**
  * MODULES
  */
-var express = require("express"),
-    bodyParser = require('body-parser'),
-    multer = require('multer'),
-    jade = require("jade");
+var express          = require("express"),
+    expressValidator = require('express-validator'),
+    bodyParser       = require('body-parser'),
+    multer           = require('multer'),
+    fs               = require('fs'),
+    jade             = require("jade");
 
 // Initialize Express object
 var app = express();
@@ -22,9 +27,10 @@ var root = __dirname,
  * CONFIGURATION
  */
 app.use(express.static(root + "/public"));
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(multer()); // for parsing multipart/form-data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressValidator());
+app.use(multer());
 
 app.set("views", root + "/views");
 app.set("view engine", "jade");
@@ -33,18 +39,47 @@ app.locals.pretty = true; // pretty html output
 /**
  * ROUTES
  */
-app.get("/settings/profile", function helloCallback(req, res) {
+app.get("/profile", function profileCb(req, res) {
+
+    fs.readFile('data.json', function (err, data) {
+        if (err) throw err;
+
+        var profileData = JSON.parse(data);
+
+        res.render("view", profileData);
+    });
+
+});
+
+app.get("/settings/profile", function editProfileCb(req, res) {
     res.render("edit");
 });
 
-app.post("/settings/profile", function postProfileCb(req, res) {
-    console.log(req.body);
+app.post("/settings/profile", function postEditProfileCb(req, res) {
 
-    return res.json({
-        "name"    : req.body.name,
-        "surname" : req.body.surname,
-        "bio"     : req.body.bio,
-    })
+    req.assert('name', 'Name field is required').notEmpty();
+    req.assert('surname', 'Surname field is required').notEmpty();
+    req.assert('bio', 'Biography field is required').notEmpty();
+
+    var errors = req.validationErrors();
+
+    if (!errors) {
+        fs.writeFile('data.json', JSON.stringify(req.body, null, 2), function (err) {
+
+            if (err) throw err;
+
+            console.log('Data was saved!');
+
+            res.redirect("/profile");
+        });
+    }
+    else {
+        res.render("edit", {
+            fieldErrors: errors,
+            fieldsData: req.body
+        });
+    }
+
 });
 
 // Start server on port 3000
